28170f34904b2c942767eb1f8d9d137b48538135
huanghengxin <957169693@qq.com>
2021-07-07 14:06:41 +0800

    20210707

---
+++ order/order-details/components/order-information/constants.js
@@ -0,0 +1,16 @@
+//订单详情 订单信息
+export const ORDER_DETAILS_ORDER_INFORMATION = [
+  { name1: '订单类型：', code1: 'orderType', name2: '合同主体：', code2: 'contractPart' },
+  {
+    name1: '签单人员：',
+    code1: 'signBillPerson',
+    name2: '合同编号：',
+    code2: 'contractNumber',
+  },
+  { name1: '用户名称：', code1: 'userName', name2: '下单方式：', code2: 'orderWay' },
+  { name1: '用户账号：', code1: 'userAccount', name2: '下单金额：', code2: 'orderMoney' },
+  { name1: '下单区域：', code1: 'orderArea', name2: '实付金额：', code2: 'practicalMoney' },
+  { name1: '下单时间：', code1: 'orderTime', name2: '待付金额：', code2: 'waitMoney' },
+  { name1: '支付时间：', code1: 'payTime', name2: '完成时间：', code2: 'finishTime' },
+  { name1: '备注：', code1: 'remark' },
+];
